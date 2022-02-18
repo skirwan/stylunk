fbbf025b5bb0c1183ceac88bbf7f7ac2ed307f42
Sean Kerwin <skerwin@mac.com>
2022-02-18 00:59:15 -0800

    Remove `KnownBerries`.

--- src/CLData/Berries.ts
+++ src/CLData/Berries.ts
@@ -7,30 +7,16 @@ export interface Berry {
     bleach: ColorManipulator
 }
 
-interface KnownBerries {
-    lila: Berry;
-    orga: Berry;
-    bramble: Berry;
-}
-
-export const Berries: Array<Berry> & KnownBerries = (()=>{
-    let lila = {
-        name: 'Lilaberry',
-        dye: new DyeBleachBase('+L', [{ component: 'r', change: -1 }]),
-        bleach: new DyeBleachBase('-L', [{ component: 'g', change: 1 }, { component: 'b', change: 1 }])
-    };
-
-    let orga = {
-        name: 'Orgaberry',
-        dye: new DyeBleachBase('+O', [{ component: 'g', change: -1 }]),
-        bleach: new DyeBleachBase('-O', [{ component: 'r', change: 1 }, { component: 'b', change: 1 }])
-    };
-
-    let bramble = {
-        name: 'Brambleberry',
-        dye: new DyeBleachBase('+B', [{ component: 'b', change: -1 }]),
-        bleach: new DyeBleachBase('-B', [{ component: 'r', change: 1 }, { component: 'g', change: 1 }])
-    };
-
-    return Object.assign( [lila, bramble, orga], { lila, orga, bramble });
-})()
+export const Berries: Array<Berry> = [{
+    name: 'Lilaberry',
+    dye: new DyeBleachBase('+L', [{ component: 'r', change: -1 }]),
+    bleach: new DyeBleachBase('-L', [{ component: 'g', change: 1 }, { component: 'b', change: 1 }])
+}, {
+    name: 'Orgaberry',
+    dye: new DyeBleachBase('+O', [{ component: 'g', change: -1 }]),
+    bleach: new DyeBleachBase('-O', [{ component: 'r', change: 1 }, { component: 'b', change: 1 }])
+}, {
+    name: 'Brambleberry',
+    dye: new DyeBleachBase('+B', [{ component: 'b', change: -1 }]),
+    bleach: new DyeBleachBase('-B', [{ component: 'r', change: 1 }, { component: 'g', change: 1 }])
+}];
