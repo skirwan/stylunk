1ab82c654f77d47586a9579e7b454c62290cf351
Sean Kerwin <skerwin@mac.com>
2022-02-19 13:43:03 -0800

    Add secret test berries

--- src/CLData/Berries.ts
+++ src/CLData/Berries.ts
@@ -7,7 +7,7 @@ export interface Berry {
     bleach: ColorManipulator
 }
 
-export const Berries: Array<Berry> = [{
+let b: Array<Berry> = [{
     name: 'Lilaberry',
     dye: new DyeBleachBase('+L', [{ component: 'r', change: -1 }]),
     bleach: new DyeBleachBase('-L', [{ component: 'g', change: 1 }, { component: 'b', change: 1 }])
@@ -20,3 +20,23 @@ export const Berries: Array<Berry> = [{
     dye: new DyeBleachBase('+B', [{ component: 'b', change: -1 }]),
     bleach: new DyeBleachBase('-B', [{ component: 'r', change: 1 }, { component: 'g', change: 1 }])
 }];
+
+if (window.location.hash === '#SECRET') {
+    b = b.concat([
+        {
+            name: 'Rageberry',
+            dye: new DyeBleachBase('+[RG]', [{ component: 'r', change: -1 }, { component: 'g', change: -1 }]),
+            bleach: new DyeBleachBase('-[RG]', [{ component: 'b', change: 1 }])
+        }, {
+            name: 'Rubberberry',
+            dye: new DyeBleachBase('+[RB]', [{ component: 'r', change: -1 }, { component: 'b', change: -1 }]),
+            bleach: new DyeBleachBase('-[RB]', [{ component: 'g', change: 1 }])
+        }, {
+            name: 'Gooberberry',
+            dye: new DyeBleachBase('+[GB]', [{ component: 'g', change: -1 }, { component: 'b', change: -1 }]),
+            bleach: new DyeBleachBase('-[GB]', [{ component: 'r', change: 1 }])
+        }
+    ]);
+}
+
+export const Berries = b;
