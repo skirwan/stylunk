358f6357e002dbc175659ce388cd036c9b0ab740
Sean Kerwin <skerwin@mac.com>
2022-02-18 22:50:57 -0800

    Hide nav chrome on iOS.

--- src/index.tsx
+++ src/index.tsx
@@ -15,3 +15,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+window.setTimeout(() => window.scrollTo(0, 1), 0);
